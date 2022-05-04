36f4bb09301a0d029e4689b949a865b7c4b4dd89
Maksim <semimaks@mail.ru>
2022-05-04 21:29:15 +0300

    Правка кода

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ def get_data():
     response = req.json()
     # print(response)
     sell_price = response['btc_rur']['sell']
-    print(f"Time: {datetime.now().strftime('%Y-%M-%D %H:%M')}\nSell BTC price: {sell_price}")
+    print(f"Текущее время: {datetime.now().strftime('%Y-%M-%D %H:%M')}\nСтоимость битка в руб: {sell_price}")
 
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
 
-    @bot.message_handler(commands=['start'])
+    @bot.message_handler(commands=['старт'])
     def start_message(message):
-        bot.send_message(message.chat.id, "Привет, напиши 'price' для поиска цены BTC")
+        bot.send_message(message.chat.id, "Привет, напиши 'биток' для поиска цены BTC")
 
     @bot.message_handler(content_types=['text'])
     def send_text(message):
-        if message.text.lower() == 'price':
+        if message.text.lower() == 'биток':
             try:
                 req = requests.get('https://yobit.net/api/3/ticker/btc_rur')
                 response = req.json()
                 sell_price = response['btc_rur']['sell']
                 bot.send_message(
                     message.chat.id,
-                    f"Time: {datetime.now().strftime('%Y-%M-%D %H:%M')}\nSell BTC price: {sell_price}"
+                    f"Текущее время: {datetime.now().strftime('%Y-%M-%D %H:%M')}\nСтоимость битка в руб: {sell_price}"
                 )
             except Exception as err:
                 print(err)
